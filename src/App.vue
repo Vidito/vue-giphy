444092c4616a0c65ea3be89d9b34497f2a8de8a2
CodeSandbox <support@codesandbox.io>
2023-12-05 01:55:56 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <search-input @gifs-fetched="onGifsFetched"/>
+
+    <gif-list :gifs="gifs"/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+
+import SearchInput from "./components/SearchInput.vue";
+import GifList from "./components/GifList.vue";
+
+export default {
+  name: 'App',
+  components: {
+    GifList,
+    SearchInput
+  },
+  data() {
+    return {
+      gifs: [],
+    }
+  },
+  methods: {
+    onGifsFetched(result) {
+      console.log(result);
+      this.gifs = result.data
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+body {
+  padding: 20px;
+  font-family: sans-serif;
+  background: #f5f5f5;
+}
+
+#app {
+  width: 800px;
+  margin: 0 auto;
 }
 </style>
